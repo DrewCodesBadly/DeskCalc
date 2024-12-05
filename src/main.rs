4a7a13348b9e8ab40b7d38b86fb5ecdb626484dc
DrewCodesBadly <dhruvak@outlook.com>
2024-12-05 16:48:18 -0500

    fix crate attribute so terminal doesn't open

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![windows_subsystem = "windows"]
+
 use core::f32;
 use eframe::{run_native, App, CreationContext, NativeOptions};
 use egui::{CentralPanel, Id, Label, Layout, RichText, TextEdit, TopBottomPanel};
